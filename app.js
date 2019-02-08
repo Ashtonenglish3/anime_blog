80c5ba3b3e8559702eb6053d9e92f8ca5b2bfe30
Ashton <ashtonenglish3@gmail.com>
2019-02-08 11:10:35 +0200

    added a summernote

--- app.js
+++ app.js
@@ -2,26 +2,32 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs')
 
 
-
-app.set('view engine', 'ejs');
-
-var routes = require('./routes');
-
-var path = require('path');
+var routes = require('./routes')
+//routes
+var path = require('path')
 app.use(express.static(path.join(__dirname, 'public')));
-///routes
-
 
 //home
-app.get('/', routes.index);
-app.get('/', partials.nav);
 
-// app.get('/darth', function(req, res) {
-//     res.send("This is a server  response on the darth page");
-// })
+app.get('/', routes.index);
+///create
+app.get('/create',(req,res) =>{
+    res.render('create');
+});
+
+///to make a new page
+app.get('/anime/:episode_number?', routes.anime_single);
+
+// app.get('/darth', function(req,res) {
+//     res.send("This is a server  darth home");
+// });
 
 app.listen(3000, function() {
-    console.log("The application is running on localhost:3000");
+    console.log("This app is running on localhost:3000");
 });
+
+
+//NOT FOUND
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,38 @@
-var moviesJSON = require('../db.json');
+var animeJSON = require('../db.json')
 
 
 //home
+
 exports.index = function(req, res) {
-    
-    var anime = moviesJSON.anime;
-    var about = moviesJSON.about;
+
+    var anime = animeJSON.anime;
+    var about = animeJSON.about;
 
     res.render('index', {
-        title : "Anime",
+        title : "anime",
+        anime :anime,
         title2 : "Wars",
         title3 : "Welcome to my blog",
         title4  : "About",
         anime : anime,
-        about : about
-
+        about :about,
     });
-  };
+};
+
+///to make a new page
+exports.anime_single = function(req, res){
+
+    var episode_number = req.params.episode_number;
+
+    var anime = animeJSON.anime;
+
+    var animes = anime[episode_number - 1];
+
+    var title =  animes.title;
+
+    res.render('anime_single', {
+         anime : anime,
+         title : title,
+         animes : animes
+    });
+    };
